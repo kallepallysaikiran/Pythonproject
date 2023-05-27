e4ddeb2bc43015ceb1e569caffd6afca7457de6e
kallepallysaikiran <134003319+kallepallysaikiran@users.noreply.github.com>
2023-05-27 12:18:59 +0530

    Update .py

--- .py
+++ .py
@@ -4,10 +4,23 @@
 #first detect a face
 from deepface import Deepface
 import matplotlib.pyplot as plt
+import time
 #using tensorflow backend
-img_path="deepface/tests/dataset/img1.jpg"
+img1_path="deepface/tests/dataset/img1.jpg"
+img1_path="deepface/tests/dataset/img1.jpg"
+
 detectors=['opencv','ssd','dlib','mtcnn']
 for dectecor in detectors:
-  print(detectors)
+  obj=DeepFace.varify(img1_path,img2_path,detector_backend=detector)
+  tic=time.time()
 
 img=Deepface.detectFace(img_path=img_path,detector_backend=detector)
+toc=time.time()
+
+plt.imshow(img1)
+plt.show()
+plt.imshow(img2)
+plt.show()
+print(obj)
+print("detector,"backend",toc-tic,"seconds")
+print("+++++++++++++++++++++")
